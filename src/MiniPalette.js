adb5f95b67f9f7f38dbe9c88bf576fad9b3ca0b9
Milly Uylenbroek <m.uylenbroek@eagerly.nl>
2022-01-10 13:47:12 +0100

    add uuid for minipalette colors

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,14 +1,16 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
+import { v4 as uuid } from 'uuid';
 import { withStyles } from '@mui/styles';
 import styles from './styles/MiniPaletteStyles';
+
 
 // in stead of using <Link> we change the history with the goToPalette function.
 // this way we don't have to use div's in an anchor link.
 function MiniPalette(props) {
   const { classes, colors, emoji, paletteName: name, goToPalette } = props;
   const colorDivs = colors.map(color => (
-    <div key={color.id} className={classes.box} style={ { backgroundColor: color.color }}></div>
+    <div key={uuid()} className={classes.box} style={ { backgroundColor: color.color }}></div>
   ));
   return (
     <div className={classes.root} onClick={goToPalette}>
